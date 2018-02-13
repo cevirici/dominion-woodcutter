176967aff8bc04ede837990267a6031ffd59c0a8
ceviri <bleh3@hotmail.com>
2018-02-13 23:39:38 +0800

    Updated root urls.py

--- ceviri_net/urls.py
+++ ceviri_net/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/woodcutter/input')),
     path('admin/', admin.site.urls),
     path('woodcutter/', include('woodcutter.urls')),
 ]
