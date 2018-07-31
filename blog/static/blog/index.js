0354c9f6034798d83f5efaf0b8d952367896499f
ceviri <bleh3@hotmail.com>
2018-07-31 21:56:13 +0800

    Blog sexy hovering

--- blog/static/blog/index.js
+++ blog/static/blog/index.js
@@ -1,4 +1,41 @@
 function toggle(post){
     $(post).toggleClass("active");
-    $(post).children(".post-body").toggleClass("hidden");
+    var expandBar = $(post).find(".post-body-full")
+    if (expandBar.attr('compressed') == '0'){
+        compress(expandBar);
+    } else {
+        expand(expandBar);
+    }
+}
+
+
+function compress(post){
+    var actualHeight = post.prop('scrollHeight');
+    var tempTransition = post.css('transition');
+    post.css('transition', '');
+
+    requestAnimationFrame(function(){
+        post.css('height', actualHeight + 'px');
+        post.css('transition', tempTransition);
+
+        requestAnimationFrame(function(){
+            post.css('height', '0px');
+        });
+    });
+
+    $(post).attr('compressed', '1');
+    $(post).toggleClass('compressed');
+}
+
+
+function expand(post){
+    var actualHeight = post.prop('scrollHeight') + parseInt(post.css('margin-bottom'));
+    post.css('height', actualHeight + 'px');
+    post[0].addEventListener('transitioned', function(e){
+        post[0].removeEventListener('transitioned', arguments.callee);
+        post.css('height', null);
+    });
+
+    $(post).attr('compressed', '0');
+    $(post).toggleClass('compressed');
 }
