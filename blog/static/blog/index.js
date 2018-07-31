412912613a76cfcc54df91017495be7ec33ec499
ceviri <bleh3@hotmail.com>
2018-07-31 22:55:15 +0800

    Blog PS

--- blog/static/blog/index.js
+++ blog/static/blog/index.js
@@ -11,31 +11,38 @@ function toggle(post){
 
 function compress(post){
     var actualHeight = post.prop('scrollHeight');
-    var tempTransition = post.css('transition');
-    post.css('transition', '');
 
     requestAnimationFrame(function(){
         post.css('height', actualHeight + 'px');
-        post.css('transition', tempTransition);
 
         requestAnimationFrame(function(){
             post.css('height', '0px');
+            post[0].addEventListener('transitionend', function(e){
+                post[0].removeEventListener('transitionend', arguments.callee);
+                $(post).addClass('compressed');
+            });
         });
     });
 
     $(post).attr('compressed', '1');
-    $(post).toggleClass('compressed');
 }
 
 
 function expand(post){
     var actualHeight = post.prop('scrollHeight') + parseInt(post.css('margin-bottom'));
     post.css('height', actualHeight + 'px');
-    post[0].addEventListener('transitioned', function(e){
-        post[0].removeEventListener('transitioned', arguments.callee);
-        post.css('height', null);
+    post[0].addEventListener('transitionend', function(e){
+        post[0].removeEventListener('transitionend', arguments.callee);
+        post.css('height', '');
     });
 
     $(post).attr('compressed', '0');
-    $(post).toggleClass('compressed');
+    $(post).removeClass('compressed');
 }
+
+
+const psmain = new PerfectScrollbar('.scroll-container', {suppressScrollX:true});
+
+$(window).resize(function(){
+    psmain.update();
+})
